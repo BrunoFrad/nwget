1bcd413291d1e645d38e767dca339b98d0d4578b
BrunoFrad <brunfradrib@gmail.com>
2026-01-06 20:41:31 -0300

    style: improve CLI UX

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@
 import { Command } from "commander";
 import { downloadFile } from "./lib.js";
 import * as path from "path";
+import colors from "ansi-colors";
 
 const program = new Command().name("nwget").version("0.1.0");
 
@@ -11,6 +12,11 @@ program
     .option("-o, --output <output>", "Path to download the file")
     .action(async (url, options) => {
         try {
+            console.log("\n",
+                        colors.magenta(`Starting the download from: `),
+                        colors.cyan(url),
+                        "\n");
+
             let filePath;
 
             if (options.output) {
@@ -28,9 +34,11 @@ program
                 filePath = path.resolve(process.cwd(), fileName);
             }
 
-            await downloadFile(url, filePath);
+            await downloadFile(url, filePath).then(() => console.log("\n", colors.green("Downloaded successfully!")));
+
+            console.log("\n");
         } catch (err) {
-            console.error("An error occurred: ", err.message);
+            console.error(colors.red("\n\nAn error occurred: "), err.message, "\n");
             process.exit(1);
         }
     }).parse();
--- src/lib.js
+++ src/lib.js
@@ -1,13 +1,36 @@
 import { pipeline } from "stream/promises";
+import { Transform } from "stream";
 import * as fs from "fs";
+import * as cliProgress from "cli-progress";
 
 export async function downloadFile(url, path) {
     const res = await fetch(url);
 
+    const fileSize = Number(res.headers.get('content-length'));
+    const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+    progressBar.start(fileSize, 0);
+
     if (!res.ok) throw new Error("Failed to download file");
+
+    let downloaded = 0;
+
+    const progressStreamTransform = new Transform({
+       transform(chunk, encoding, callback) {
+            downloaded += chunk.length;
+
+            progressBar.update(downloaded);
+
+            if (progressBar.getProgress() === 1)
+                progressBar.stop();
+
+            callback(null, chunk);
+       }
+    });
 
     await pipeline(
         res.body,
+        progressStreamTransform,
         fs.createWriteStream(path)
     );
 }
